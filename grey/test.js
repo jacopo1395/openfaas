11c0f88f4989b62f0ba753adaebd705a053d46a0
Jacopo <jack1395@hotmail.it>
2018-05-02 23:45:40 +0200

    grey

---
+++ grey/test.js
@@ -0,0 +1,19 @@
+const download = require('image-downloader')
+var Jimp = require("jimp");
+
+const options = {
+    url: "http://www.personal.psu.edu/oeo5025/jpg.jpg",
+    dest: './image.jpg'
+}
+download.image(options)
+.then(({ filename, image }) => {
+    Jimp.read(filename, function(err, img) {
+        if (err) throw err;
+        img.greyscale() // set greyscale
+            .write("img.jpg"); // save
+        // callback(undefined, {status: "done"});
+    });
+
+}).catch((err) => {
+    // callback(err, {status: "error"});
+})
