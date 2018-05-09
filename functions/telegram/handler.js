c4255604899cebc6fd7f330e0e75d635ff0e5db3
Jacopo <jack1395@hotmail.it>
2018-05-09 17:05:47 +0200

    snowboy

--- functions/telegram/handler.js
+++ functions/telegram/handler.js
@@ -1,7 +1,7 @@
 "use strict"
 
 var request = require('request');
-var openfaas = "https://maker.ifttt.com/trigger/openfaas/with/key/dnya7-3BCob77ewMbq_bbe";
+var openfaas = "https://maker.ifttt.com/trigger/openfaas/with/key/diGWR1we397BBxqRdvMS3X";
 
 module.exports = (context, callback) => {
     request.post({url:openfaas, form: {value1:context}}, function (error, response, body) {
--- functions/weather/handler.js
+++ functions/weather/handler.js
@@ -5,13 +5,13 @@ module.exports = (context, callback) => {
     var url = "http://api.openweathermap.org/data/2.5/weather?q="+context+"&appid=2299edbf49e2c97b3b3a7f7e78669fb7";
     request(url, function (error, response, body) {
         var jsonbody = JSON.parse(body);
-        // var weather = jsonbody.weather[0].main;
+        var weather = jsonbody.weather[0].main;
         // console.log(weather);
         // var json = {};
         // json.status="ok";
         // json.err=error;
         // json.res=response;
         // json.body=body;
-        callback(undefined, jsonbody);
+        callback(undefined, weather);
     });
 }
