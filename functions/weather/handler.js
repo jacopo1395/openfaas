cc6ff34b144796a6421edfa79fe0c8adebabd649
Jacopo <jack1395@hotmail.it>
2018-05-09 20:31:34 +0200

    vocal commands

--- functions/weather/handler.js
+++ functions/weather/handler.js
@@ -2,16 +2,17 @@
 var request = require('request');
 
 module.exports = (context, callback) => {
-    var url = "http://api.openweathermap.org/data/2.5/weather?q="+context+"&appid=2299edbf49e2c97b3b3a7f7e78669fb7";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+context+"&units=metric&appid=2299edbf49e2c97b3b3a7f7e78669fb7";
     request(url, function (error, response, body) {
         var jsonbody = JSON.parse(body);
-        var weather = jsonbody.weather[0].main;
+        // var weather = jsonbody.weather[0].description;
         // console.log(weather);
         // var json = {};
         // json.status="ok";
         // json.err=error;
         // json.res=response;
         // json.body=body;
-        callback(undefined, weather);
+        // json.response=weather;
+        callback(undefined, jsonbody);
     });
 }
--- vocal/demo.py
+++ vocal/demo.py
@@ -1,6 +1,10 @@
 import snowboydecoder
 import sys
 import signal
+import requests
+import os
+import subprocess
+
 
 interrupted = False
 
@@ -14,21 +18,43 @@ def interrupt_callback():
     global interrupted
     return interrupted
 
-if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
+def weather():
+    r = requests.post('http://127.0.0.1:8080/function/weather', data='roma')
+    # print(r.json())
+    res = r.json()['weather'][0]['description']
+    # print(res)
+    temp = str(r.json()['main']['temp'])
+    res = 'in Rome now there is '+res+' and the temperature is '+temp+' degrees celsius'
+    cmd = 'say '+res
+    print (res)
+    # subprocess.check_output(['say', res])
+    os.system(cmd)
+
+def telegram():
+    r = requests.post('http://127.0.0.1:8080/function/telegram', data='Hi everyone!')
+    print ('ok, I send you a message')
+    os.system('say "ok, I send you a message"')
+
+if len(sys.argv) != 3:
+    print("Error: need to specify 2 model names")
+    print("Usage: python demo.py 1st.model 2nd.model")
     sys.exit(-1)
 
-model = sys.argv[1]
+models = sys.argv[1:]
 
 # capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
-detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
+w = weather
+t = telegram
+
+sensitivity = [0.5]*len(models)
+detector = snowboydecoder.HotwordDetector(models, sensitivity=0.5)
 print('Listening... Press Ctrl+C to exit')
 
+
 # main loop
-detector.start(detected_callback=snowboydecoder.play_audio_file,
+detector.start(detected_callback=[w,t],
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
